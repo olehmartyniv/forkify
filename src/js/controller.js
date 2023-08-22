35ac2b43390a7d74502817c0216f81ab3e98e443
Oleh Martyniv <oleh.martyniv@gmail.com>
2023-08-22 15:03:33 +0300

    added publisher-subscriber

--- src/js/controller.js
+++ src/js/controller.js
@@ -27,6 +27,8 @@ const controlRecipes = async function () {
   }
 };
 
-['hashchange', 'load'].forEach(event =>
-  window.addEventListener(event, controlRecipes)
-);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+
+init();
