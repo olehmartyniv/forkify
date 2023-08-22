48a14f3a254fd73d440fc8d325d34fd66a8a3411
Oleh Martyniv <oleh.martyniv@gmail.com>
2023-08-22 17:39:42 +0300

    error and success messages added

--- src/js/controller.js
+++ src/js/controller.js
@@ -23,12 +23,11 @@ const controlRecipes = async function () {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (error) {
-    console.log(error);
+    recipeView.renderError();
   }
 };
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
 };
-
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -22,6 +22,6 @@ export const loadRecipe = async function (id) {
       ingredients: recipe.ingredients,
     };
   } catch (err) {
-    console.log(`${err} 💥💥💥💥`);
+    throw err;
   }
 };
