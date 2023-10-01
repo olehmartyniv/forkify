b93203f93c40b832db64405b4b2aa9655a9ff804
Oleh Martyniv <oleh.martyniv@gmail.com>
2023-10-01 20:29:22 +0300

    fixed Fraction

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,45 +1,45 @@
 import View from './View';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+const { Fraction } = require('fractional');
 
 class RecipeView extends View {
-  _parentElement = document.querySelector('.recipe');
-  _errorMessage = 'We could not find that recipe. Please try another one!';
-  _message = '';
+	_parentElement = document.querySelector('.recipe');
+	_errorMessage = 'We could not find that recipe. Please try another one!';
+	_message = '';
 
-  addHandlerRender(handler) {
-    ['hashchange', 'load'].forEach(event =>
-      window.addEventListener(event, handler)
-    );
-  }
+	addHandlerRender(handler) {
+		['hashchange', 'load'].forEach((event) =>
+			window.addEventListener(event, handler)
+		);
+	}
 
-  addHandlerUpdateServings(handler) {
-    this._parentElement.addEventListener('click', function (e) {
-      const btn = e.target.closest('.btn--update-servings');
+	addHandlerUpdateServings(handler) {
+		this._parentElement.addEventListener('click', function (e) {
+			const btn = e.target.closest('.btn--update-servings');
 
-      if (!btn) return;
+			if (!btn) return;
 
-      const updateTo = +btn.dataset.updateTo;
-      if (updateTo > 0) handler(updateTo);
-    });
-  }
+			const updateTo = +btn.dataset.updateTo;
+			if (updateTo > 0) handler(updateTo);
+		});
+	}
 
-  addHandlerAddBookmark(handler) {
-    this._parentElement.addEventListener('click', function (e) {
-      const btn = e.target.closest('.btn--bookmark');
+	addHandlerAddBookmark(handler) {
+		this._parentElement.addEventListener('click', function (e) {
+			const btn = e.target.closest('.btn--bookmark');
 
-      if (!btn) return;
+			if (!btn) return;
 
-      handler();
-    });
-  }
+			handler();
+		});
+	}
 
-  _generateMarkup() {
-    return `
+	_generateMarkup() {
+		return `
       <figure class="recipe__fig">
         <img src="${this._data.image}" alt="${
-      this._data.title
-    }" class="recipe__img" />
+			this._data.title
+		}" class="recipe__img" />
         <h1 class="recipe__title">
           <span>${this._data.title}</span>
         </h1>
@@ -51,8 +51,8 @@ class RecipeView extends View {
             <use href="${icons}#icon-clock"></use>
           </svg>
           <span class="recipe__info-data recipe__info-data--minutes">${
-            this._data.cookingTime
-          }</span>
+						this._data.cookingTime
+					}</span>
           <span class="recipe__info-text">minutes</span>
         </div>
         <div class="recipe__info">
@@ -60,21 +60,21 @@ class RecipeView extends View {
             <use href="${icons}#icon-users"></use>
           </svg>
           <span class="recipe__info-data recipe__info-data--people">${
-            this._data.servings
-          }</span>
+						this._data.servings
+					}</span>
           <span class="recipe__info-text">servings</span>
 
           <div class="recipe__info-buttons">
             <button class="btn--tiny btn--update-servings" data-update-to="${
-              this._data.servings - 1
-            }">
+							this._data.servings - 1
+						}">
               <svg>
                 <use href="${icons}#icon-minus-circle"></use>
               </svg>
             </button>
             <button class="btn--tiny btn--update-servings" data-update-to="${
-              this._data.servings + 1
-            }">
+							this._data.servings + 1
+						}">
               <svg>
                 <use href="${icons}#icon-plus-circle"></use>
               </svg>
@@ -90,8 +90,8 @@ class RecipeView extends View {
         <button class="btn--round btn--bookmark">
           <svg class="">
             <use href="${icons}#icon-bookmark${
-      this._data.bookmarked ? '-fill' : ''
-    }"></use>
+			this._data.bookmarked ? '-fill' : ''
+		}"></use>
           </svg>
         </button>
       </div>
@@ -108,8 +108,8 @@ class RecipeView extends View {
         <p class="recipe__directions-text">
           This recipe was carefully designed and tested by
           <span class="recipe__publisher">${
-            this._data.publisher
-          }</span>. Please check out
+						this._data.publisher
+					}</span>. Please check out
           directions at their website.
         </p>
         <a
@@ -124,26 +124,26 @@ class RecipeView extends View {
         </a>
       </div>
     `;
-  }
+	}
 
-  #generateMarkupIngredient(ingredients) {
-    return `
+	#generateMarkupIngredient(ingredients) {
+		return `
       <li class="recipe__ingredient">
         <svg class="recipe__icon">
           <use href="${icons}#icon-check"></use>
         </svg>
         <div class="recipe__quantity">${
-          ingredients.quantity
-            ? new Fraction(ingredients.quantity).toString()
-            : ''
-        }</div>
+					ingredients.quantity
+						? new Fraction(ingredients.quantity).toString()
+						: ''
+				}</div>
         <div class="recipe__description">
           <span class="recipe__unit">${ingredients.unit ?? ''}</span>
           ${ingredients.description}
         </div>
       </li>
     `;
-  }
+	}
 }
 
 export default new RecipeView();
